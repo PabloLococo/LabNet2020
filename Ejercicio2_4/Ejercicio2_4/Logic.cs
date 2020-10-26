bdd73ee7dc0e1efcf46a7570398d031d6a9ded7c
PabloLococo <64929975+PabloLococo@users.noreply.github.com>
2020-10-25 22:51:11 -0300

    Add files via upload

---
+++ Ejercicio2_4/Ejercicio2_4/Logic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2_4
+{
+ public   class Logic
+    {
+        private List<Producto> productos = new List<Producto>();
+
+        public List<Producto> Productos { get 
+            {
+                return productos;
+            }
+        }
+
+        public void IngresarProductos(string nombre, int precio)
+        {
+            if (precio == 0 | precio > 50000)
+            {
+                throw new PrecioErroException();
+            }
+            else
+            {
+                productos.Add(new Producto(precio, nombre));
+            }
+        }
+    }
+}
