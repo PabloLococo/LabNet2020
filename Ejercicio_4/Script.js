7b9c156aa8cdd2df6ea25c9297034db3119e86ca
Pablo Lococo <pablolococo14@gmail.com.ar>
2020-11-09 14:44:59 -0300

    Ejercicio_4

---
+++ Ejercicio_4/Script.js
@@ -0,0 +1,8 @@
+function ValidadorTexto()
+{  
+    if(   document.getElementById("Nombre").value  == "" 
+       || document.getElementById("Apellido").value== "")
+        {           
+         alert("No ingreso documentos")
+        }          
+}
