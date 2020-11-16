dc7557700a463463334671bc3700768437f9400d
Pablo Lococo <pablolococo14@gmail.com.ar>
2020-11-16 16:56:12 -0300

    Puede hacer el ejercicio de api pero tarde en ver el problema de porque no me lo mostraba era que no lo habia especificado a los label, no lo corregi porque estoy a 5 minutos de la clase pero lo corrijo cuando termina la clase

---
+++ Ejercicio_6_1/Controllers/StarshipController.cs
@@ -0,0 +1,52 @@
+﻿using Ejercicio_6_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicio_6_1.Controllers
+{
+    public class StarshipController : Controller
+    {
+        // GET: Starship
+        public ActionResult Index()
+        {
+            var starshipViewModel = new StarshipViewModel();
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.dev/api/starships/");
+            var responseTalk = client.GetAsync("9");
+            responseTalk.Wait();
+            var result = responseTalk.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<StarshipViewModel>();
+                readTask.Wait();
+
+                starshipViewModel = readTask.Result;
+            }
+            return View(starshipViewModel);
+        }
+
+
+        [HttpGet]
+        public ActionResult GetStartship()
+        {
+            var starshipViewModel = new StarshipViewModel();
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://swapi.dev/api/starships/");
+            var responseTalk = client.GetAsync("9");
+            responseTalk.Wait();
+            var result = responseTalk.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<StarshipViewModel>();
+                readTask.Wait();
+                
+                starshipViewModel = readTask.Result;
+            }
+            return RedirectToAction("Starship");
+        }
+    }
+}
