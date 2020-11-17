47687d6479b8ce4d421fe51f3b7f0380cf85ffa5
Pablo Lococo <pablolococo14@gmail.com.ar>
2020-11-16 22:27:34 -0300

    Arregle el problema y hay tiene un ejercicio completo, no voy a poder hacer mas porque el miercoles tengo parcial y tengo que estudiar. Muchas gracias por todo

--- Ejercicio_6_1/App_Start/RouteConfig.cs
+++ Ejercicio_6_1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Ejercicio_6_1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Starship", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- Ejercicio_6_1/Controllers/StarshipController.cs
+++ Ejercicio_6_1/Controllers/StarshipController.cs
@@ -11,32 +11,12 @@ namespace Ejercicio_6_1.Controllers
     public class StarshipController : Controller
     {
         // GET: Starship
-        public ActionResult Index()
-        {
+        public ActionResult Index(string starshipNumber)
+        {          
             var starshipViewModel = new StarshipViewModel();
             var client = new HttpClient();
             client.BaseAddress = new Uri("https://swapi.dev/api/starships/");
-            var responseTalk = client.GetAsync("9");
-            responseTalk.Wait();
-            var result = responseTalk.Result;
-            if (result.IsSuccessStatusCode)
-            {
-                var readTask = result.Content.ReadAsAsync<StarshipViewModel>();
-                readTask.Wait();
-
-                starshipViewModel = readTask.Result;
-            }
-            return View(starshipViewModel);
-        }
-
-
-        [HttpGet]
-        public ActionResult GetStartship()
-        {
-            var starshipViewModel = new StarshipViewModel();
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("https://swapi.dev/api/starships/");
-            var responseTalk = client.GetAsync("9");
+            var responseTalk = client.GetAsync(starshipNumber);
             responseTalk.Wait();
             var result = responseTalk.Result;
             if (result.IsSuccessStatusCode)
@@ -46,7 +26,14 @@ namespace Ejercicio_6_1.Controllers
                 
                 starshipViewModel = readTask.Result;
             }
-            return RedirectToAction("Starship");
+            return View(starshipViewModel);
         }
+
+
+        //[HttpGet]
+        //public ActionResult GetStartship(string starshipNumber)
+        //{
+
+        //}
     }
 }
